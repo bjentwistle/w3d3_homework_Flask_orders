b5cc644a4db5a6b1d2582baa7a1bcc0491a9f4b6
Becky <bjentwistle@gmail.com>
2023-04-19 21:47:05 +0100

    tried to add some css styles

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,8 +1,12 @@
 from app import app
 from flask import render_template
 from models.toyshop_list import orders
-
+from models.toyshop_list import order1
 
 @app.route('/orders') 
 def index():   #should display all of the orders on the page
     return render_template("index.jinja", title = "Toy's Home", orders = orders)
+
+@app.route('/orders/id')
+def one_order():
+    return render_template("single_order.jinja", title = "One order", order = order1)
